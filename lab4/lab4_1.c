8f711b126280c0f0023a8511c74bbb84eb13b92e
avolnation <55879986+avolnation@users.noreply.github.com>
2019-11-01 15:41:13 +0300

    Update and rename lab4_1 to lab4_1.c

---
+++ lab4/lab4_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    
+    int h,i;
+    int a[10]={1,2,3,4,5,6,7,8,9,10};
+
+    for(i=0;i<=9;i++)
+    {
+        h=9-i;
+        a[i]=a[h-i];
+    }
+    for(i=0;i<=9;i++){
+    printf("%d ", a[i]);
+    }
+  return 0;
+ 
+}
