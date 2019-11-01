97be4c0d1959ac9c5249178681816a362e4b7419
avolnation <55879986+avolnation@users.noreply.github.com>
2019-11-01 14:13:04 +0300

    Update and rename lab1_1 to lab1_1.c

---
+++ lab1/lab1_1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+ int main(void) {
+   int x,res;
+   printf("Введите переменную x:");
+   scanf("%d",&x);
+   res=16*x*x;
+   printf("Результат:%d\n",res);
+   return 0;
+ }
