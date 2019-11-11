37a0e58cf59a3832d3d623bd901730e615a8849d
avolnation <55879986+avolnation@users.noreply.github.com>
2019-11-11 23:09:54 +0300

    Create lab2_2.c

---
+++ lab2/lab2_2.c
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdio.h>
+int main() {
+	
+	int a,b,c,count; //deklariruem peremennie
+	
+	
+	
+	printf("Vvedite 3 chisla (a,b,c) \n"); 
+	
+	scanf("%d %d %d", &a, &b, &c); //poluchaem znacheniya peremennih a,b,c
+	
+	count = 0; //prisvaem schetchiky polozhitelnih chisel znachenie 0
+	
+	if(a>0){
+		
+	    count++;  //proveryaem a,b,c na polozhitelnost
+			
+	}
+	
+	if(b>0){
+		
+		count++;	
+	}
+	
+	if(c>0){
+		
+		count++;
+	}
+	
+	
+	printf("Chislo polozhitelnih chisel = %d", count);   //vivodim chislo polozhitelnih peremennih
+	
+	return 0;
+}
