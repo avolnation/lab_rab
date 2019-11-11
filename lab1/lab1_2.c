2c0989f9706dd90b57473a85154aa1a7ec734cf1
avolnation <55879986+avolnation@users.noreply.github.com>
2019-11-11 22:54:24 +0300

    Update lab1_2.c

--- lab1/lab1_2.c
+++ lab1/lab1_2.c
@@ -7,12 +7,12 @@ int main(void) {
 	
 	float x,y,z,a,b,c,d,s; //декларируем переменные
 	
-	
+	printf("Vvedite zmachenija peremennix x, y, z ");
 	scanf("%f \n %f \n %f", &x, &y, &z); //считываем значения переменных с клавиатуры
 	a = pow(9+(pow((x-y),2)),1.0/3.0); 
 	b = (pow(x,2.0)+pow(y,2.0))+2;
 	c = exp(fabs(x-y))*pow(tan(z),3.0);
-	s = (a/b)-c; //ðàññ÷èòûâàþ ðåçóëüòàò
+	s = (a/b)-c; //рассчитываем результат
 	
 
     printf("%f \n",s); //выводим результат на экран
