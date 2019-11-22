1475b3859266ae3212116f0e9a0baef3b4c81f37
avolnation <55879986+avolnation@users.noreply.github.com>
2019-11-22 15:32:01 +0300

    Create lab5_2.c

---
+++ lab5/lab5_2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+   int i,j,k,l,m,n;
+
+   printf("Vvedite kolichestvo strok:");
+   scanf("%d", &n);
+   printf("Vvedite kolichestvo stolbcov:");
+   scanf("%d", &m);
+    
+    
+   int a[n][m];
+
+   
+   for(i=0;i<n;i++){
+       for(j=0;j<m;j++){
+       scanf("%d", &a[i][j]);
+    }
+
+    for(i=0;i<n;i++){
+       
+
+    }
+
+
+
+}
+}
