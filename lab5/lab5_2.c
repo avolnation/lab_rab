5639ce796acd6e373795bf69cfe25abb022fd253
avolnation <55879986+avolnation@users.noreply.github.com>
2019-11-26 21:38:14 +0300

    Update lab5_2.c

--- lab5/lab5_2.c
+++ lab5/lab5_2.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(){
 
-   int i,j,k,l,m,n;
+   int i,j,k,h,m,n,maxi,mini,maxj,minj;
 
    printf("Vvedite kolichestvo strok:");
    scanf("%d", &n);
@@ -14,17 +13,45 @@ int main(){
    int a[n][m];
 
    
-   for(i=0;i<n;i++){
-       for(j=0;j<m;j++){
+   for(i=0;i<n;i++)
+   {
+       for(j=0;j<m;j++)
+	   {
        scanf("%d", &a[i][j]);
+       }
+   }
+        
+      
+    for(i=0;i<n;i++)
+	{
+       for(j=0;j<m;j++)
+	   {
+	   	
+	   	minj=mini=1e9;
+	   	maxj=maxi=-1e9;
+	
+          for(k=0;k<n;k++)
+		  {
+		  	 if(a[k][j]<mini) 
+		  	 mini=a[k][j];
+		  	 if(a[k][j]>maxi) 
+		  	 maxi=a[k][j];	
+		  }
+		  for(h=0;h<m;h++)
+		  {
+		  	if(a[i][h]<minj) 
+		  	minj = a[i][h];
+		  	if(a[i][h]>maxj) 
+		  	maxj = a[i][h];
+		  }
+		if(a[i][j] == mini && a[i][j]==maxj) 
+		  printf("i: %d j: %d\n",i,j);
+        if(a[i][j] == maxi && a[i][j] == minj) 
+          printf("i: %d j: %d\n",i,j);
+	   
+
+       }
     }
-
-    for(i=0;i<n;i++){
-       
-
-    }
-
-
-
-}
+    
+return 0;
 }
