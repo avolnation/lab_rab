973f570809ab43f6e8f2950aac467f56ea37fe08
avolnation <55879986+avolnation@users.noreply.github.com>
2019-12-06 15:38:09 +0300

    Update lab6_1.c

--- lab6/lab6_1.c
+++ lab6/lab6_1.c
@@ -1,1 +1,29 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+int main(){
+    int i,n,comp,count=0;
+    int *min,*max;
+    
+    printf("Vvedite razmer massiva ");
+    scanf("%d", &n);
+    
+    int A[n];
+    int elem[n];
+    
+    for(i=0;i<n;i++){
+    scanf("%d", &A[i]);
+    }
+    
+    comp = 1;
+
+    for(min = A, max = &A[n]; min < max; min++){
+        if(*min % 2 == 0){
+            comp *= elem[count++];
+        }
+    }
+    printf("Proizvedenie chetnix elementov ravno %d", comp);
+
+    return 0;
+}
