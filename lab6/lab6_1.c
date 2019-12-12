33fc0b20a378432242b34d206a51d7509cca3dce
avolnation <55879986+avolnation@users.noreply.github.com>
2019-12-12 23:57:16 +0300

    Update lab6_1.c

--- lab6/lab6_1.c
+++ lab6/lab6_1.c
@@ -1,30 +1,41 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-int main(){
-    int i,n,comp,count=0;
-    int *min,*max;
-    
-    printf("Vvedite razmer massiva ");
-    scanf("%d", &n);
-    
-    int A[n];
-    int elem[n];
-    
-    for(i=0;i<n;i++){
-    scanf("%d", &A[i]);
-    }
-    
-    comp = 1;
+#include <math.h>
 
-    for(min = A, max = &A[n]; min < max; min++){
-        if(*min % 2 == 0){
-            elem[count++] = *min;
-            comp *= *elem;
-        }
-    }
-    printf("Proizvedenie chetnix elementov ravno %d", comp);
-
-    return 0;
-}
+int main() {
+	int i, kol=0, n, p=1, a, s=0, c, h;	//äåêëàðèðóåì íåîáõîäèìûå ïåðåìåííûå 
+	int b[10];	// äåêëàðèðóåì ìàññèâ    
+	do{
+		printf("Vvedite kol-vo elementov massiva (<10)\n");
+		scanf("%d", &n);	// ââîäèì êîëè÷åñòâî ýëåìåíòîâ ìàññèâà
+	}while (n>=10);
+	
+	for(i=0; i<n; i++){
+		printf("Vvedite element [%d]\n",i);	// ââîäèì ýëåìåíòû ìàññèâà
+		scanf("%d", b+i);
+	}
+	printf("\n");
+	
+	for (i=0;i<n;i++) 	printf ("%d\n", *(b+i));		// ñîçäà¸ì óêàçàòåëè
+					
+	for(i=0; i<n; i++){
+		
+		if(i%2==0){	// ñòàâèì óñëîâèå
+			a=*(b+i);	//ïðèñâàèâàåì ïåðåìåííîé çíà÷åíèå óêàçàòåëÿ
+			p*=a;		// íàõîäèì ïðîèçâåäåíèå è âûâîäèì ðåçóëüòàò íà ýêðàí
+			printf("Proizvedenie elementov = %d\n", p);
+}}
+	for(i=0; i<n; i++){
+		//åñëè íà÷èíàÿ ñ ïåðâîãî ýëåìåíòà , ïåðâûé ýëåìåíò ðàâåí íóëþ, òî ïðèñâàèâàåì ïåðåìåíîé  c  çíà÷åíèå  i
+		if(*(b+i)==0){
+			c=i; 	break; }}
+			
+			for(i=n-1; i>=0; i--)	//åñëè íà÷èíàÿ ñ ïîñëåäíåãî  ýëåìåíòà , ïåðâûé ýëåìåíò ðàâåí íóëþ, òî ïðèñâàèâàåì ïåðåìåíîé h  c çíà÷åíèå  i
+			if(*(b+i)==0){
+			h=i;   break;  }
+			
+				for(i=c; i<=h; i++){
+			s+=*(b+i);}	// íàõîäèì ñóììó
+			printf("Summa elementov = %d\n", s);
+			getch(); }
