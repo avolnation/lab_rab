42cd654d1f55493a186c781b7d6c9da006edfc3f
avolnation <55879986+avolnation@users.noreply.github.com>
2019-12-13 14:12:42 +0300

    Update lab6_1.c

--- lab6/lab6_1.c
+++ lab6/lab6_1.c
@@ -1,41 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
-#include <math.h>
 
-int main() {
-	int i, kol=0, n, p=1, a, s=0, c, h;	//äåêëàðèðóåì íåîáõîäèìûå ïåðåìåííûå 
-	int b[10];	// äåêëàðèðóåì ìàññèâ    
-	do{
-		printf("Vvedite kol-vo elementov massiva (<10)\n");
-		scanf("%d", &n);	// ââîäèì êîëè÷åñòâî ýëåìåíòîâ ìàññèâà
-	}while (n>=10);
+int main(){
 	
-	for(i=0; i<n; i++){
-		printf("Vvedite element [%d]\n",i);	// ââîäèì ýëåìåíòû ìàññèâà
-		scanf("%d", b+i);
+	int *ptr,i,n,dvs=1,firstNull,lastNull,sum=0;
+	
+	printf("Vvedite chislo elementov ");
+	scanf("%d", &n);
+	
+	if(!(ptr=(int*)malloc(n*sizeof(int))))
+	{ puts("Not enough memory");
+	return 1;	
 	}
-	printf("\n");
 	
-	for (i=0;i<n;i++) 	printf ("%d\n", *(b+i));		// ñîçäà¸ì óêàçàòåëè
-					
-	for(i=0; i<n; i++){
-		
-		if(i%2==0){	// ñòàâèì óñëîâèå
-			a=*(b+i);	//ïðèñâàèâàåì ïåðåìåííîé çíà÷åíèå óêàçàòåëÿ
-			p*=a;		// íàõîäèì ïðîèçâåäåíèå è âûâîäèì ðåçóëüòàò íà ýêðàí
-			printf("Proizvedenie elementov = %d\n", p);
-}}
-	for(i=0; i<n; i++){
-		//åñëè íà÷èíàÿ ñ ïåðâîãî ýëåìåíòà , ïåðâûé ýëåìåíò ðàâåí íóëþ, òî ïðèñâàèâàåì ïåðåìåíîé  c  çíà÷åíèå  i
-		if(*(b+i)==0){
-			c=i; 	break; }}
+	for(i=0;i<n;i++){
+		printf("el[%d]", i+1);
+		scanf("%d",ptr+i);
+	}
+	for(i=0;i<n;i++){
+		if(i % 2 == 0){
+			dvs *= *(ptr+i);
 			
-			for(i=n-1; i>=0; i--)	//åñëè íà÷èíàÿ ñ ïîñëåäíåãî  ýëåìåíòà , ïåðâûé ýëåìåíò ðàâåí íóëþ, òî ïðèñâàèâàåì ïåðåìåíîé h  c çíà÷åíèå  i
-			if(*(b+i)==0){
-			h=i;   break;  }
-			
-				for(i=c; i<=h; i++){
-			s+=*(b+i);}	// íàõîäèì ñóììó
-			printf("Summa elementov = %d\n", s);
-			getch(); }
+		}
+	}
+	
+	printf("%d\n",dvs);
+	
+	for(i=0;i<n;i++){
+	    if(*(ptr+i) == 0){
+	    	 firstNull = i;
+	    	 break;
+		}			
+		}
+		printf("fN = %d\n", firstNull);
+	for(i=n;i>firstNull;i--){
+		if(*(ptr+i) == 0){
+			lastNull = i;
+		}	
+	}
+		printf("lN = %d\n", lastNull);
+	
+	for(i=firstNull;i<lastNull;i++){
+		sum += *(ptr+i);
+	}	
+	
+	printf("%d", sum);
+	free(ptr);
+	return 0;
+}
