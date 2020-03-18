1d711567557a12c96d17c594e4c7c829723a4997
avolnation <55879986+avolnation@users.noreply.github.com>
2020-03-18 13:18:14 +0300

    Update metod_simple_iterations.c

--- lab11/metod_simple_iterations.c
+++ lab11/metod_simple_iterations.c
@@ -1,30 +1,37 @@
 #define i 3
-#define j 3
+#define j 4
 #include <stdio.h>
 #include <math.h>
 
-    float matrix[i][j] = {{2.5,-3.12,-4.03},
-                           {0.61,0.71,-0.05},
-                            {-1.03,-2.05,0.87}};
-    float freeMatrix[i][1] = {{-7,5},
-                              {0.44},
-                              {-1,16}};
-    float matrixIterative[i][j] = {{},{},{}};
+    float matrix[i][j] = {{2.5,-3.12,-4.03,-7.5},
+                           {0.61,0.71,-0.05,0.44},
+                            {-1.03,-2.05,0.87,-1.16}};
 
+
+void printMatrix(void){
+    int k,l;
+     for(k=0;k<i;k++){
+         for(l=0;l<j;l++){
+              printf("%f ",matrix[k][l]);
+         }
+         printf("\n");
+     }
+ }
  void iterativeTransition(void){
      int k,l;
-     for(k=i;k<i;k++){
-         for(l=j;l<j;l++){
-              matrixIterative[k][l] = ((-matrix[k][l])/matrix[k][k]);
+     for(k=0;k<i;k++){
+         for(l=0;l<j;l++){
+              matrix[k][l] = ((-matrix[k][l])/matrix[k][k]);
          }
      }
+     printMatrix();
  }
  void convergence(void){
      int k,l;
      float sum,rootFromSum;
-     for(k=i;k<i;k++){
-         for(l=j;l<j;l++){
-             sum += matrixIterative[k][l];
+     for(k=0;k<i;k++){
+         for(l=0;l<j-1;l++){
+             sum += matrix[k][l];
          }
      }
     rootFromSum = sqrt(sum);
@@ -34,22 +41,7 @@
         printf("Root From Sum !< 1");
  }
  }
- void printMatrix(void){
-    int k,l;
-     for(k=i;k<i;k++){
-         for(l=j;l<j;l++){
-              printf("%8.4f",matrixIterative[k][l]);
-         }
-         printf("\n");
-     }
- }
  int main (){
-     
      iterativeTransition();
-     printMatrix();
-
-
-
-
-    return 0;
+     return 0;
 }
