f0cc1ed1400a4bd9340692cf7b1881a65c4ee81f
avolnation <55879986+avolnation@users.noreply.github.com>
2020-03-25 13:49:45 +0300

    Update rab.c

--- lab12/rab.c
+++ lab12/rab.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <math.h>
 
+
+
+int HashFunction(int k){
+    return(k%10);
+}
+    void main(){
+
+        int key;
+
+        printf("Key = ");
+        scanf("%d\n", &key);
+        printf("Hash Function(%d)=%d",key,HashFunction(key));
+    }
